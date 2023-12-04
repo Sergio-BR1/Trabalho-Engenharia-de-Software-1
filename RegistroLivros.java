b22dfdd0603324943f07fac0ff0786eebd823ce9
Sergio-BR1 <71470010+Sergio-BR1@users.noreply.github.com>
2023-12-04 12:34:14 +0000

    Update Singleton RegistroLivros

--- RegistroLivros.java
+++ RegistroLivros.java
@@ -4,7 +4,39 @@ public class RegistroLivros {
 	private ArrayList<Livro> livrosDisponiveis;
 	private ArrayList<Livro> livrosAlugados;
 
-	private RegistroLivros() {};
+	private RegistroLivros() {
+		livrosDisponiveis = new ArrayList<Livro>();
+		livrosAlugados = new ArrayList<Livro>();
+	};
+
+	public static RegistroLivros getInstancia() {
+		if (instancia == null) {
+			instancia = new RegistroLivros();
+		}
+		return instancia;
+	}
+
+	public void adicionarLivroDisponivel(Livro livro) {
+		livrosDisponiveis.add(livro);
+	}
+
+	public void adicionarLivroAlugado(Livro livro) {
+		livrosAlugados.add(livro);
+		livrosDisponiveis.remove(livro);
+	}
+	public static void setInstancia(RegistroLivros instancia) {
+		RegistroLivros.instancia = instancia;
+	}
+
+	public ArrayList<Livro> getLivrosDisponiveis() {
+		return livrosDisponiveis;
+	}
+
+
+	public ArrayList<Livro> getLivrosAlugados() {
+		return livrosAlugados;
+	}
+	
 
 	public static RegistroLivros obterInstancia() {
 		if (instancia == null) {
